0b0ce3dbda6c103df1d251ad2d08455f0d2624a4
Indu Ramesh <induramesh@gmail.com>
2019-07-31 04:25:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,16 @@ class School
   end 
 
   def add_student(name, grade)
-    roster["name"] = grade 
+    roster[grade] = [name]
+  end 
+  
+  def grade(grade)
+    roster[grade]
+  end 
+  
+  def sort
+    sorted_roster = roster.map { |grade, names| names.sort }
+    sorted_roster
+  end 
   
 end 
